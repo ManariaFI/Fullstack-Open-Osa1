4c9bf1859000cf0df1c51d980e1ecd75bcc248d1
Melissa Erin <146720736+ManariaFI@users.noreply.github.com>
2023-11-03 09:40:12 +0200

    Add files via upload

---
+++ unicafe/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const StatisticLine = ({ value, text }) => (
+    <tr>
+        <td>{text}</td>
+        <td> </td>
+        <td>{value}</td>
+    </tr>
+);
+
+const Statistics = (props) => {
+    if (props.all === 0) {
+        return <div>No feedback given</div>;
+    }
+    return (
+        <table>
+            <tbody>
+                <StatisticLine text="good" value={props.good} />
+                <StatisticLine text="neutral" value={props.neutral} />
+                <StatisticLine text="bad" value={props.bad} />
+                <StatisticLine
+                    text="average"
+                    value={(props.good - props.bad) / props.all}
+                />
+                <StatisticLine
+                    text="positive"
+                    value={(props.good / props.all) * 100 + " %"}
+                />
+            </tbody>
+        </table>
+    );
+};
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [all, setAll] = useState(0);
+
+    const handleGood = () => {
+        setGood(good + 1);
+        setAll(all + 1);
+    };
+    const handleNeutral = () => {
+        setNeutral(neutral + 1);
+        setAll(all + 1);
+    };
+    const handleBad = () => {
+        setBad(bad + 1);
+        setAll(all + 1);
+    };
+
+    return (
+        <div>
+            <div>
+                <h1>give feedback</h1>
+                <Button handleClick={handleGood} text="good" />
+                <Button handleClick={handleNeutral} text="neutral" />
+                <Button handleClick={handleBad} text="bad" />
+                <h1>statistics</h1>
+                <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+            </div>
+        </div>
+    );
+};
+
+export default App;
